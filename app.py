1f5d8b57382f0bb064ae0d1eaf5b3b2557bd894d
raf-pads <74691095+raf-pads@users.noreply.github.com>
2020-11-18 21:32:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -171,4 +171,6 @@ app.layout = html.Div(
 )
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8051, host='0.0.0.0')
+    app.run_server(debug=True)
+    
+# , port=8051, host='0.0.0.0'
